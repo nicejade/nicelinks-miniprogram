902e22d317e1afc3ab6db68b15c1f66ee502abd3
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2021-07-21 00:17:58 +0800

    :sparkles: preload AD - CoooL

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <script>
 export default {
   created() {
+    // 广告预加载 https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/ad/preload-ad.html#
+    wx.preloadAd([
+      {
+        unitId: 'adunit-b8597298ad9815ee', // banner广告广告单元
+        type: 'banner' // banner广告
+      },
+      {
+        unitId: 'adunit-9ddfb625edc2f4c9', // banner广告广告单元
+        type: 'banner' // banner广告
+      },
+      {
+        unitId: 'adunit-bf7192a16fd82d8a', // banner广告广告单元
+        type: 'banner' // banner广告
+      },
+      {
+        unitId: 'adunit-70ae2fde86f2c5a1', // banner广告广告单元
+        type: 'banner' // banner广告
+      },
+      {
+        unitId: 'adunit-b2c629edc8a1fa6f', // 视频广告广告单元
+        type: 'video' // 视频广告
+      }
+    ])
     // 调用API从本地缓存中获取数据
     /*
      * 平台 api 差异的处理方式:  api 方法统一挂载到 mpvue 名称空间, 平台判断通过 mpvuePlatform 特征字符串
